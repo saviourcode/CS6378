9877738af6663d0f3521dcebc176c15843570a90
Sourabh Choure <sourabhchoure98@gmail.com>
2021-10-04 19:37:03 -0500

    broken

--- ClientHandler.java
+++ ClientHandler.java
@@ -14,28 +14,23 @@ class ClientHandler extends Thread {
     }
 
     public void run() {
-        try {
-            DataInputStream inStream = new DataInputStream(serverClient.getInputStream());
-            System.out.println(inStream);
-            byte[] data = new byte[100];
-            inStream.read(data);
-            System.out.println("Passed this block " + data);
-            Message msg = new Message(clientNodeID, data);
-            listener.receive(msg);
-            // while(!clientMessage.equals("bye")){
-            // clientMessage=inStream.readUTF();
-            // System.out.println("From Client-" +clientNo+ ": Number is :"+clientMessage);
-            // squre = Integer.parseInt(clientMessage) * Integer.parseInt(clientMessage);
-            // serverMessage="From Server to Client-" + clientNo + " Square of " +
-            // clientMessage + " is " +squre;
-            // outStream.writeUTF(serverMessage);
-            // outStream.flush();
-            // }
-            inStream.close();
-        } catch (Exception ex) {
-            System.out.println(ex);
-        } finally {
-
+        while(true)
+        {
+            try {
+                DataInputStream inStream = new DataInputStream(serverClient.getInputStream());
+                System.out.println(inStream);
+                byte[] data = new byte[100];
+                inStream.read(data);
+                System.out.println("Passed this block " + data);
+                Message msg = new Message(clientNodeID, data);
+                listener.receive(msg);
+                inStream.close();
+            } catch (Exception ex) {
+                listener.broken(clientNodeID);
+                System.out.println(ex);
+                break;
+            }
         }
+        
     }
 }
