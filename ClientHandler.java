d05be0ca72ebfc5f25f399fe6405f6cffac2d166
Sourabh Choure <sourabhchoure98@gmail.com>
2021-10-04 19:07:36 -0500

    New Changes

--- ClientHandler.java
+++ ClientHandler.java
@@ -7,35 +7,35 @@ class ClientHandler extends Thread {
     int squre;
     Listener listener;
 
-    ClientHandler(Socket inSocket,NodeID clientNodeID, Listener listener){
-      this.serverClient = inSocket;
-      this.clientNodeID = clientNodeID;
-      this.listener = listener;
+    ClientHandler(Socket inSocket, NodeID clientNodeID, Listener listener) {
+        this.serverClient = inSocket;
+        this.clientNodeID = clientNodeID;
+        this.listener = listener;
     }
 
-    public void run(){
-      try{
-        DataInputStream inStream = new DataInputStream(serverClient.getInputStream());
-        System.out.println(inStream);
-	byte[] data = new byte[100];
-        inStream.read(data);
-	System.out.println("Passed this block "+ data);
-        Message msg = new Message(clientNodeID, data);
-        listener.receive(msg);
-        // while(!clientMessage.equals("bye")){
-        //   clientMessage=inStream.readUTF();
-        //   System.out.println("From Client-" +clientNo+ ": Number is :"+clientMessage);
-        //   squre = Integer.parseInt(clientMessage) * Integer.parseInt(clientMessage);
-        //   serverMessage="From Server to Client-" + clientNo + " Square of " + clientMessage + " is " +squre;
-        //   outStream.writeUTF(serverMessage);
-        //   outStream.flush();
-        // }
-        inStream.close();
-        serverClient.close();
-      }catch(Exception ex){
-        System.out.println(ex);
-      }finally{
-        
-      }
+    public void run() {
+        try {
+            DataInputStream inStream = new DataInputStream(serverClient.getInputStream());
+            System.out.println(inStream);
+            byte[] data = new byte[100];
+            inStream.read(data);
+            System.out.println("Passed this block " + data);
+            Message msg = new Message(clientNodeID, data);
+            listener.receive(msg);
+            // while(!clientMessage.equals("bye")){
+            // clientMessage=inStream.readUTF();
+            // System.out.println("From Client-" +clientNo+ ": Number is :"+clientMessage);
+            // squre = Integer.parseInt(clientMessage) * Integer.parseInt(clientMessage);
+            // serverMessage="From Server to Client-" + clientNo + " Square of " +
+            // clientMessage + " is " +squre;
+            // outStream.writeUTF(serverMessage);
+            // outStream.flush();
+            // }
+            inStream.close();
+        } catch (Exception ex) {
+            System.out.println(ex);
+        } finally {
+
+        }
     }
-  }
+}
